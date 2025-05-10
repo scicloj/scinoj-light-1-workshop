0a8c9bbd4cf6f440b1739d9a07e9ca2158125b66
daslu <daniel.slutsky@gmail.com>
2025-05-10 13:49:06 +0300

    data WIP

--- notebooks/scinoj_light_1/workshop/group_A/data.clj
+++ notebooks/scinoj_light_1/workshop/group_A/data.clj
@@ -1,6 +1,8 @@
 (ns scinoj-light-1.workshop.group-A.data
   (:require [tablecloth.api :as tc]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.math :as math]
+            [java-time.api :as java-time]))
 
 (def raw-projects
   (tc/dataset "data/ks-projects-201801.csv.gz"
@@ -33,3 +35,41 @@ raw-projects
     (tc/random 5))
 
 (keyword "category")
+
+(first 
+ [4 1 4])
+
+
+(tc/info raw-projects)
+
+
+(:launched raw-projects)
+
+
+(math/log10 1000)  ; 10*10*10 = 1000
+
+(map math/log10
+ [1 10 100 1000 10000])
+
+
+(def processed-projects
+  (-> raw-projects
+      (tc/map-columns :log10goal
+                      [:usd_goal_real]
+                      math/log10)
+      (tc/map-columns :back-ratio
+                      [:usd_pledged_real :backers]
+                      (fn [u b]
+                        (if (zero? b)
+                          nil
+                          (/ u b))))
+      (tc/map-columns :launched
+                      [:launched]
+                      #(java-time/local-date-time "yyyy-MM-dd HH:mm:ss" %))))
+
+
+(java-time/local-date-time
+ "yyyy-MM-dd HH:mm:ss"
+ "2016-01-13 18:13:53")
+
+
