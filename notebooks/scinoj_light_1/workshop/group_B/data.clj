78b5eeac563e94e9a32be8f2e5431f598bf4f547
daslu <daniel.slutsky@gmail.com>
2025-05-10 20:19:54 +0300

    data WIP

--- notebooks/scinoj_light_1/workshop/group_B/data.clj
+++ notebooks/scinoj_light_1/workshop/group_B/data.clj
@@ -2,7 +2,8 @@
 
 (ns scinoj-light-1.workshop.group-B.data
   (:require [tablecloth.api :as tc]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [java-time.api :as java-time]))
 
 
 (def raw-projects
@@ -34,3 +35,55 @@ raw-projects
 
 
 (tc/info raw-projects)
+
+
+(- (+ (* 1 1000) 200) 30)
+
+(-> 1
+    (* 1000)
+    (+ 200)
+    (- 30))
+
+(macroexpand-1
+ '(-> 1
+    (* 1000)
+    (+ 200)
+    (- 30)))
+
+(- (+ (* 1 1000) 200) 30)
+
+
+raw-projects
+
+
+(tc/random 
+ (tc/select-columns raw-projects
+                    [:name :category :usd_goal_real])
+ 5
+ {:seed 1})
+
+
+(-> raw-projects
+    (tc/select-columns [:name :category :usd_goal_real])
+    (tc/random 5 {:seed 1}))
+
+
+(count "ABCDEF")
+
+(-> raw-projects
+    (tc/map-columns :name-length
+                    [:name]
+                    count)
+    (tc/select-columns [:name :name-length]))
+
+
+(java-time/local-date-time "yyyy-MM-dd HH:mm:ss" 
+                           "2015-08-11 12:12:28")
+
+(def processed-projects
+  (-> raw-projects
+      (tc/map-columns :launched
+                      [:launched]
+                      (partial java-time/local-date-time
+                               "yyyy-MM-dd HH:mm:ss"))))
+
