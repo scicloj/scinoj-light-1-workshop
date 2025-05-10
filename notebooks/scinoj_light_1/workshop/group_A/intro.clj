b3406caf560f2fd4904bcd981330081bcf885811
daslu <daniel.slutsky@gmail.com>
2025-05-10 13:18:06 +0300

    renamed directory

---
+++ notebooks/scinoj_light_1/workshop/group_A/intro.clj
@@ -0,0 +1,63 @@
+(ns scinoj-light-1.workshop.group-A.intro)
+
+;; https://www.kaggle.com/datasets/kemical/kickstarter-projects
+
+(def data 
+  [{:ID 244511278,
+    :name "Tomárâho Dictionary",
+    :category "Publishing",
+    :main_category "Publishing",
+    :currency "USD",
+    :deadline "2013-06-20",
+    :goal 7000.0,
+    :launched "2013-05-21 01:31:04",
+    :pledged 2675.0,
+    :state "failed",
+    :backers 37,
+    :country "US",
+    :usd_pledged 2675.0,
+    :usd_pledged_real 2675.0,
+    :usd_goal_real 7000.0}
+   {:ID 2140575984,
+    :name "Porcelain doll with handmade crochet dress",
+    :category "Crochet",
+    :main_category "Crafts",
+    :currency "USD",
+    :deadline "2015-03-27", 
+    :goal 500.0, 
+    :launched "2015-02-25 23:11:03", 
+    :pledged 0.0, 
+    :state "failed",
+    :backers 0, 
+    :country "US", 
+    :usd_pledged 0.0, 
+    :usd_pledged_real 0.0,
+    :usd_goal_real 500.0}])
+
+(type data)
+
+(vector? data)
+
+(data 0)
+
+(first data)
+
+(type (first data))
+
+(map? (first data))
+
+(keys (first data))
+
+(second (keys (first data)))
+
+(type (second (keys (first data))))
+
+(:category (first data))
+
+(-> data first :category)
+
+;; -> is a macro
+
+(macroexpand
+ ' (-> data first :category))
+
