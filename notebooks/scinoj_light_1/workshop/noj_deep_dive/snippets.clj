ed28848ce8e8915f792f25956ecf6c9a1372689e
daslu <daniel.slutsky@gmail.com>
2025-05-16 16:53:04 +0300

    deepdive WIP

--- notebooks/scinoj_light_1/workshop/noj_deep_dive/snippets.clj
+++ notebooks/scinoj_light_1/workshop/noj_deep_dive/snippets.clj
@@ -1,24 +1,31 @@
 (ns scinoj-light-1.workshop.noj-deep-dive.snippets
   (:require [tablecloth.api :as tc]
             [scicloj.metamorph.ml.rdatasets :as rdatasets]
-            [tablecloth.column.api :as tcc]))
+            [tablecloth.column.api :as tcc]
+            [scicloj.tableplot.v1.plotly :as plotly]
+            [fastmath.random :as random]
+            [tech.v3.datatype :as dt]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## A little example
 
+(let [n 100]
+  (-> {:x (range n)}
+      tc/dataset
+      (tc/add-column :y
+                     (fn [{:keys [x]}]
+                       (tcc/+ 9
+                              (->> (dt/make-reader :float32 n (random/grand))
+                                   (reductions +))
+                              (tcc/* 0.1 x)
+                              (tcc/sq (tcc/* 0.1 (tcc/- x 50))))))
+      tc/dataset
+      plotly/layer-line
+      (plotly/layer-smooth {:=design-matrix [[:x '(identity :x)]
+                                             [:x2 '(* :x :x)]]})))
 
-
-
-
-
-
-
-
-
-
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; hamf example 1 --- faster drop-in replacements for many operations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## hamf example 1 --- faster drop-in replacements for many operations
 (time (->> (range 10000000)
            (shuffle)
            (partition-all 10000)
@@ -35,7 +42,7 @@
            (count)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; hamf example 2 --- sets with faster operations (that still respect clojure equality)
+;; ## hamf example 2 --- sets with faster operations (that still respect clojure equality)
 
 (def s (java.util.HashSet.))
 (.add s (long 1))
@@ -55,8 +62,8 @@ s
 (time (def _ (set (range 1000000))))
 (time (def _ (hamf/mut-set (range 1000000))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; hamf example 3 --- similar with maps
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## hamf example 3 --- similar with maps
 (def m (java.util.HashMap.))
 (.put m (long 1) "uh")
 (.put m (int 1) "oh")
@@ -67,13 +74,15 @@ m
 (hamf/assoc! hm (int 1) "oh")
 hm
 
-;;;;;;;;;;;;;;;;;;;;
-;; dtype - png bytes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## dtype - png bytes
 (require '[tech.v3.libs.buffered-image :as bufimg])
 
-(bufimg/load "https://raw.githubusercontent.com/cnuernber/dtype-next/master/test/data/test.jpg")
+(def i
+  (bufimg/load "https://raw.githubusercontent.com/cnuernber/dtype-next/master/test/data/test.jpg"))
 
-(def i *1)
+i
+
 (require '[tech.v3.datatype :as dt])
 
 (dt/shape i)
@@ -89,8 +98,8 @@ hm
 
 ;; Note, uint8 is also a nice type for representing this data, typically much less convenient to work with on the JVM
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; dtype - dfn/+ between ds columns
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## dtype - dfn/+ between ds columns
 (require '[tech.v3.dataset :as ds])
 
 (def ds (ds/->>dataset {:a [1 2 3]
@@ -101,6 +110,7 @@ ds
 
 (dfn/+ (:a ds) (:b ds))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ## tech.ml.dataset / Tablecloth
 
 ;; The usual joy of Clojure is about processing JSON-like data.
@@ -108,10 +118,6 @@ ds
 
 ;; Here, we consoldate the two: processing rows as plain maps
 ;; through an efficient view of a columnwise memory representation:
-
-(require '[tablecloth.api :as tc]
-         '[scicloj.metamorph.ml.rdatasets :as rdatasets]
-         '[tablecloth.column.api :as tcc])
 
 (-> (rdatasets/datasets-iris)
     (tc/map-columns :petal-geometric-mean
@@ -123,4 +129,26 @@ ds
     (tc/group-by [:species])
     (tc/aggregate {:n tc/row-count}))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## Metamorph.ml
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## Tableplot
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ## Future hopes
+
+;; - compatibility
+
+;; - ergonomics (https://scicloj.github.io/tablemath/)
+
+;; - interop
+
+;; - [std.lang](https://scicloj.github.io/stdlang-docs/)
 
